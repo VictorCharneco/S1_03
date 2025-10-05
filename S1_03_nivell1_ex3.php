bedc69daa77b63a59123b213a1bf86b10a050f13
Victor C.B <victor.charneco.barba@gmail.com>
2025-10-05 11:39:47 +0200

    nivell1_ex3 done

--- S1_03_nivell1_ex3.php
+++ S1_03_nivell1_ex3.php
@@ -1,4 +1,15 @@
 <?php
 
+function validacioChar(array $paraules, string $caracter){
+    foreach ($paraules as $paraula){
+        if (stripos($paraula, $caracter) === false){
+            return false;
+        }
+    }
+    return true;
+}
+
+$paraules = ["hola", "Php", "Html"];
+echo validacioChar($paraules, "h");
 
 ?>
